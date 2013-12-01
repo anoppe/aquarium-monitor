7926afe407129df87e97bf376b556f9356bface3
Auke Noppe <auke@noppe.nl>
2013-12-01 14:27:15 +0100

    websockets bijna aan de gang

--- aquarium-monitor-rest/src/main/java/nl/noppe/auke/aquarium/config/AquariumConfiguration.java
+++ aquarium-monitor-rest/src/main/java/nl/noppe/auke/aquarium/config/AquariumConfiguration.java
@@ -1,23 +1,34 @@
 package nl.noppe.auke.aquarium.config;
 
+import java.util.Arrays;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 import nl.noppe.auke.aquarium.metrics.system.SystemMetricsCollector;
-import nl.noppe.auke.aquarium.tasks.TaskScheduler;
+import nl.noppe.auke.aquarium.tasks.MetricsCollectScheduler;
 import nl.noppe.auke.aquarium.web.MetricsController;
 
+import org.apache.catalina.Context;
+import org.apache.tomcat.websocket.server.WsSci;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 import com.jezhumble.javasysmon.JavaSysMon;
 
 @Configuration
 @EnableAutoConfiguration
-//@EnableScheduling
-@ComponentScan(basePackageClasses={MetricsController.class, AquariumConfiguration.class})
-public class AquariumConfiguration {
+@EnableScheduling
+@ComponentScan(basePackageClasses={MetricsCollectScheduler.class, MetricsController.class, AquariumConfiguration.class})
+public class AquariumConfiguration implements SchedulingConfigurer {
 
 	public static void main(String[] args) {
 		
@@ -27,10 +38,10 @@ public class AquariumConfiguration {
 //		SpringApplication.run(new Object[] {AquariumConfiguration.class, AquariumServiceConfiguration.class});
 	}
 
-//	@Bean
-//	public TaskScheduler taskScheduler() {
-//		return new TaskScheduler();
-//	}
+	@Bean
+	public MetricsCollectScheduler metricsCollectScheduler() {
+		return new MetricsCollectScheduler();
+	}
 	
 	@Bean
 	public SystemMetricsCollector systemMetricsCollector() {
@@ -41,5 +52,34 @@ public class AquariumConfiguration {
 	public JavaSysMon monitor() {
 		return new JavaSysMon();
 	}
+	
+	@Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        taskRegistrar.setScheduler(taskExecutor());
+    }
+
+    @Bean(destroyMethod="shutdown")
+    public Executor taskExecutor() {
+        return Executors.newScheduledThreadPool(5);
+    }
+    
+    @Bean
+    public EmbeddedServletContainerFactory tomcatEmbeddedServletContainerFactory() {
+    	TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+    	factory.setTomcatContextCustomizers(Arrays.asList(new TomcatContextCustomizer[] {
+    			tomcatContextCustomizer()
+    	}));
+    	return factory;
+    }
+
+    @Bean 
+    public TomcatContextCustomizer tomcatContextCustomizer() {
+    	return new TomcatContextCustomizer() {
+    		@Override
+    		public void customize(Context context) {
+    			context.addServletContainerInitializer(new WsSci(), null);
+    		}
+    	};
+    }
 	
 }
--- aquarium-monitor-rest/src/main/java/nl/noppe/auke/aquarium/tasks/MetricsCollectScheduler.java
+++ aquarium-monitor-rest/src/main/java/nl/noppe/auke/aquarium/tasks/MetricsCollectScheduler.java
@@ -7,14 +7,22 @@ import nl.noppe.auke.aquarium.metrics.system.SystemMetricsCollector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.scheduling.annotation.Scheduled;
 
-public class TaskScheduler {
+public class MetricsCollectScheduler {
 
-	private static final Logger logger = LoggerFactory.getLogger(TaskScheduler.class);
+	private static final Logger logger = LoggerFactory.getLogger(MetricsCollectScheduler.class);
 	
 	private SystemMetricsCollector systemMetricsCollector;
 	private MetricsProcessor metricsProcessor;
+	
+	SimpMessageSendingOperations messagingTemplate;
+	
+	@Autowired
+	public void setMessagingTemplate(SimpMessageSendingOperations messagingTemplate) {
+		this.messagingTemplate = messagingTemplate;
+	}
 	
 	@Autowired
 	public void setSystemMetricsCollector(SystemMetricsCollector systemMetricsCollector) {
@@ -31,7 +39,9 @@ public class TaskScheduler {
 		logger.debug("starting system metrics collector");
 		Metrics metrics = systemMetricsCollector.getSystemMetrics();
 		
-		metricsProcessor.processSystemMetrics(metrics);
+		messagingTemplate.convertAndSend("/queue/metrics/", metrics);
+		
+//		metricsProcessor.processSystemMetrics(metrics);
 	}
 	
 	@Scheduled(cron="*/5 * * * * ?")
